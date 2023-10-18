4a000d8ae5d8942aeac4133504ddcccf1dd85a71
camila ferreira de almeida <mila.e.ferreira28@gmail.com>
2023-10-18 11:53:29 -0300

    minicurso

---
+++ core/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import Categoria, Pais, TipoatuacaO, Produtora, Membros, Filme, Atuacao
+
+
+
+
+admin.site.register(Categoria)
+admin.site.register(Pais)
+admin.site.register(TipoatuacaO)
+admin.site.register(Produtora)
+admin.site.register(Membros)
+admin.site.register(Filme)
+admin.site.register(Atuacao)
---
+++ core/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=40)
+
+    def __str__(self)  :
+        return self.descricao
+    
+
+class Pais(models.Model):
+    nome = models.CharField(max_length=60)
+
+    def __str__(self) :
+        return self.nome
+    
+class TipoatuacaO(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self) :
+        return self.descricao
+    
+class Produtora(models.Model):
+    nome = models.CharField(max_length=100)
+    pais = models.ForeignKey(Pais, on_delete=models.PROTECT)
+
+    def __str__(self) :
+        return f"{self.nome} ({self.pais})"
+    
+class Membros(models.Model):
+    nome = models.CharField(max_length=100)
+    datanascimento = models.DateField()
+    pais = models.ForeignKey(Pais, on_delete=models.PROTECT)
+
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    ano = models.IntegerField()
+    sinopse = models.TextField()
+    duracao = models.TimeField()
+    classificacao = models.IntegerField()
+    produtor = models.ForeignKey(Produtora, on_delete=models.PROTECT)
+    categoria = models.ManyToManyField(Categoria)
+
+class Atuacao(models.Model):
+    filme = models.ForeignKey(Filme, on_delete=models.PROTECT)
+    membro = models.ForeignKey(Membros, on_delete=models.PROTECT)
+    tipo_atuacao = models.ForeignKey(TipoatuacaO, on_delete=models.PROTECT)
+    personagens = models.CharField(max_length=100, blank=True, null=True)
